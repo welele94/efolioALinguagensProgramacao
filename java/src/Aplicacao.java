66ca3b587b6d57f60da48a7470b8d3856c54f230
welele94 <2400005@estudante.uab.pt>
2026-04-29 15:35:45 +0100

    Add human comments to Aplicacao

--- java/src/Aplicacao.java
+++ java/src/Aplicacao.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Aplicacao {
     public static void main(String[] args) {
+        // O executavel OCaml pode ser indicado por argumento.
+        // Se nao for indicado, assume-se que esta na raiz do projeto.
         String executavel = args.length > 0 ? args[0] : "./main.exe";
 
+        // A aplicacao Java fica apenas com a interface e delega os calculos ao OCaml.
         IntegradorOCaml integrador = new IntegradorOCaml(executavel);
         Boletim boletim = new Boletim();
         Scanner scanner = new Scanner(System.in);
@@ -12,6 +15,7 @@ public class Aplicacao {
             Menu menu = new Menu(scanner, integrador, boletim);
             menu.iniciar();
         } finally {
+            // Fecha o Scanner no fim da aplicacao.
             scanner.close();
         }
     }
